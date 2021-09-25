2d56615266b69f9db42988c8540fa16fd9a36c9a
Kan Fu <53024471+fukan333@users.noreply.github.com>
2021-09-24 22:36:36 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,7 +15,7 @@ const siteConfig = {
     title: 'Kan Fu', // Title for your website.
     tagline: 'Continuously improving, aspiring technologist.',
     profilePhoto: 'img/profile.png',
-    url: 'https://fukan000.github.io', // Your website URL
+    url: 'https://fukan333.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'fukan000',
+    organizationName: 'fukan333',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -69,7 +69,7 @@ const siteConfig = {
     socialMedia: {
         linkedin: 'kan-fu',
         twitter: 'kf2bxs',
-        github: 'fukan000',
+        github: 'fukan333',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
